18fc5af7c41e7dc8f5073ef05380efd64cd60142
Chris鸠 <948029240@qq.com>
2021-11-04 21:17:29 +0800

    Update 1.c

--- 100-classic-cases-of-C-language/1.c
+++ 100-classic-cases-of-C-language/1.c
@@ -4,11 +4,24 @@
 程序分析：可填在百位、十位、个位的数字分别为1、2、3、4。
 					组成所有的排列以后再去不满足条件的排列。
 		*	author		:	胡必烈
-		*	codeTime 	:	2021/11/2		
+		*	codeTime 	:	2021/11/4		
 */
-#include <stdio.h>
-int main(void)
+#include <stdio.h>														//编译预处理命令
+int main(void)															//主函数
 {
-	/* code */
+	int i, j, k;																//定义变量
+
+	for (i = 1; i < 5; ++i){
+		for (j = 1; j < 5; ++j){
+			if (i != j){
+				for (k = 1; k < 5; ++k){
+					if (i != k && j != k){
+						printf("%d,%d,%d\n",i,j,k);
+					}
+				}
+			}	
+		}
+	}
+
 	return 0;
 }
