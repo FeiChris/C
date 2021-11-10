de6b68ba106ea12ae79033dfeeefc353a1f318cf
Chris鸠 <948029240@qq.com>
2021-11-10 21:12:10 +0800

    Create 2-02 输出一句话.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/2-02 输出一句话.c
@@ -0,0 +1,9 @@
+/*
+	printf("Programming is fun.\nAnd programming in C is even more fun! \n");
+*/
+#include <stdio.h>
+int main(void)
+{
+	printf("Programming is fun.\nAnd programming in C is more fun!\n");
+	return 0;
+}
