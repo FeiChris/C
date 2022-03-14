d27d32619cf6062e4bba29c9e121ff7fed2c010f
Chris鸠 <948029240@qq.com>
2022-03-15 00:18:26 +0800

    Create 习题4-8 高空坠球.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/习题4-8 高空坠球.c
@@ -0,0 +1,29 @@
+//
+//  习题4-8 高空坠球.c
+//  pat
+//
+//  Created by Vision on 2022/3/14.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    long int h,n;
+    double h1,high=0;
+    scanf("%ld %ld",&h,&n);
+    h1 = h;
+    if (n == 0) {
+        high = 0;
+        h1 = 0;
+    }else{
+        for (int i = 1; i<=n; i++) {
+            high = high + h1;
+            if (i>1) {
+                high = high + h1;
+            }
+            h1 = h1/2;
+        }
+    }
+    printf("%.1lf %.1lf",high,h1);
+    return 0;
+}
