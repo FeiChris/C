a33f9473d3f406d707f19fb6e48d19c96a4b22b5
Chris鸠 <948029240@qq.com>
2022-03-17 02:29:15 +0800

    Create 实验2-2-7 整数152的各位数字.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计实验与习题指导（第4版）》题目集/实验2-2-7 整数152的各位数字.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void)
+{
+    int Num = 152;
+    int a,b,c;
+    a = 152 % 10;
+    b = 152 / 10 % 10;
+    c = 152 / 100;
+    printf("152 = %d + %d*10 + %d*100",a,b,c);
+    return 0;
+}
