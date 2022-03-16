c77ffc28c235937071b848fd078cbf9d202ceb5a
Chris鸠 <948029240@qq.com>
2022-03-17 02:29:00 +0800

    Create 实验2-2-5 将x的平方赋值给y .c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计实验与习题指导（第4版）》题目集/实验2-2-5 将x的平方赋值给y .c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(void)
+{
+    int average,math = 87,eng = 72,comp = 93;
+    average = (math + eng + comp) / 3;
+    printf("math = 87, eng = 72, comp = 93, average = %d\n",average);
+    return 0;
+}
