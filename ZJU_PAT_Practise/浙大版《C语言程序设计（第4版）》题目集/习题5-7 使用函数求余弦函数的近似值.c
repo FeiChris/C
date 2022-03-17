91d4a2953775e9e9ddb8339b7c754ab76190714e
Chris鸠 <948029240@qq.com>
2022-03-17 14:31:41 +0800

    Create 习题5-7 使用函数求余弦函数的近似值.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/习题5-7 使用函数求余弦函数的近似值.c
@@ -0,0 +1,46 @@
+//
+//  习题5-7 使用函数求余弦函数的近似值.c
+//  PAT
+//
+//  Created by Vision on 2022/3/17.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+double funcos( double e, double x );
+
+int main()
+{
+    double e, x;
+
+    scanf("%lf %lf", &e, &x);
+    printf("cos(%.2f) = %.6f\n", x, funcos(e, x));
+    
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+double funcos( double e, double x ){
+    int flag=1,i=0;
+    double result=0,temp=1,s = 0,xia=1;
+  while (fabs(temp)>=e){
+        s = pow(x, i);
+        if(i==0)
+            xia = 1.0;
+        else{
+            xia = 1;
+            for (int j = 1; j<=i; j++) {
+                xia = xia * j;
+            }
+        }
+        temp = s / xia;
+        temp = flag * temp;
+        result = result + temp;
+        flag = -flag;
+        i = i + 2;
+    }
+    return result;
+}
+
+
