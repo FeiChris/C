2b338cb004ec09d6e09054f43cdf97029a50b4a6
Chris鸠 <948029240@qq.com>
2022-03-19 20:33:37 +0800

    Create 习题6-8 单词首字母大写.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/习题6-8 单词首字母大写.c
@@ -0,0 +1,33 @@
+//
+//  习题6-8 单词首字母大写.c
+//  PAT
+//
+//  Created by Vision on 2022/3/19.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+    int space;
+    space = 1;
+    ch = getchar();
+    while (ch != '\n') {
+        if (space == 1) {
+            if (ch >= 97 && ch <= 122) {
+                ch = ch - 32;
+            }
+            space = 0;
+        }
+        if (ch ==' ') {
+            space = 1;
+        }else{
+            space = 0;
+        }
+        
+        putchar(ch);
+        ch = getchar();
+    }
+}
+
