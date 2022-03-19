c823784180bcfe454c8ad6257f096bc32fdf0bda
Chris鸠 <948029240@qq.com>
2022-03-19 20:30:34 +0800

    Create 实验6-5 简单计算器.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计实验与习题指导（第4版）》题目集/实验6-5 简单计算器.c
@@ -0,0 +1,47 @@
+//
+//  实验6-5 简单计算器.c
+//  PAT
+//
+//  Created by Vision on 2022/3/19.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int result,flag;
+    int x;
+    char ch;
+    flag = 1;
+    scanf("%d",&x);
+    result = x;
+    ch = getchar();
+    while (ch != '=') {
+        scanf("%d",&x);
+        switch (ch) {
+            case '+':
+                result = result + x;
+                break;
+            case '-':
+                result = result - x;
+                break;
+            case '*':
+                result = result * x;
+                break;
+            case '/':
+                if (x != 0) {
+                    result = result / x;
+                }else
+                    flag = 0;
+                break;
+            default:
+                flag = 0;
+                break;
+        }
+        ch = getchar();
+    }
+    if (flag==0) {
+        printf("ERROR");
+    }else
+        printf("%d",result);
+}
