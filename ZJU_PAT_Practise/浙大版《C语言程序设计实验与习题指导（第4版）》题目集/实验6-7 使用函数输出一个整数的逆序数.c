7df5e74ae98ecc525e7000797b54275940e74525
Chris鸠 <948029240@qq.com>
2022-03-20 16:39:05 +0800

    Create 实验6-7 使用函数输出一个整数的逆序数.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计实验与习题指导（第4版）》题目集/实验6-7 使用函数输出一个整数的逆序数.c
@@ -0,0 +1,35 @@
+//
+//  实验6-7 使用函数输出一个整数的逆序数.c
+//  PAT
+//
+//  Created by Vision on 2022/3/20.
+//
+
+#include <stdio.h>
+
+int reverse( int number );
+    
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+    printf("%d\n", reverse(n));
+
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+int reverse( int number ){
+    int result=1,item;
+    result = number % 10;
+    number = number / 10;
+    
+    while (number) {
+        item = number % 10;
+        number = number / 10;
+        result = result * 10 + item;
+    }
+    
+    return result;
+}
