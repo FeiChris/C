7233d527705e8b4de9ce2aa461be5745219edd23
Chris鸠 <948029240@qq.com>
2022-03-23 22:29:35 +0800

    Create 习题7-1 选择法排序.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/习题7-1 选择法排序.c
@@ -0,0 +1,42 @@
+//
+//  习题7-1 选择法排序.c
+//  PAT
+//
+//  Created by Vision on 2022/3/23.
+//
+
+#include<stdio.h>
+#define MANX 10
+
+int main(void)
+{
+    int a[MANX];
+    int n,temp,index;
+    
+    scanf("%d", &n);
+    for (int i=0; i<n; i++) {
+        scanf("%d",&a[i]);
+    }
+    
+    for (int k = 0; k < n-1; k++) {
+        index = k;
+        for (int i=k+1; i<n; i++) {
+            if (a[i]>a[index]) {
+                index = i;
+            }
+        }
+        temp = a[index];
+        a[index] = a[k];
+        a[k] = temp;
+    }
+    
+    for (int i=0; i<n; i++) {
+        if (i==0) {
+            printf("%d",a[i]);
+        }else
+            printf(" %d",a[i]);
+    }
+    
+    
+    return 0;
+}
