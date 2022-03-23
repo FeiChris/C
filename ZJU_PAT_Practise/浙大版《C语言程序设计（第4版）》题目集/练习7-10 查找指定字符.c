d94044e613897a9df83deacb6aa87b63007d6b51
Chris鸠 <948029240@qq.com>
2022-03-23 22:17:20 +0800

    Create 练习7-10 查找指定字符.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/练习7-10 查找指定字符.c
@@ -0,0 +1,43 @@
+//
+//  练习7-10 查找指定字符.c
+//  PAT
+//
+//  Created by Vision on 2022/3/23.
+//
+
+#include<stdio.h>
+#define MANX 80
+
+int main(void)
+{
+    char line[MANX];
+    int k = 0;
+    int i = 0;
+    int index=0;
+    int flag = 1;
+    
+    char ch,c;
+    ch = getchar();
+    c = getchar();
+    while ((line[k]=getchar())!='\n') {
+        k++;
+    }
+    line[k] = '\0';
+    
+    while (i<k) {
+        if (ch == line[i]) {
+            index = i;
+            flag = 0;
+        }
+        i++;
+    }
+    
+    if (flag==1) {
+        printf("Not Found\n");
+    }else
+        printf("index = %d",index);
+    
+    return 0;
+}
+                                                             
+
