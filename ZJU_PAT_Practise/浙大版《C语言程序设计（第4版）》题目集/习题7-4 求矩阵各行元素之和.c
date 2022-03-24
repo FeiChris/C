11ca0bf5cdf219567a5c515a634632a4fd4bd08c
Chris鸠 <948029240@qq.com>
2022-03-24 22:35:51 +0800

    Create 习题7-4 求矩阵各行元素之和.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/习题7-4 求矩阵各行元素之和.c
@@ -0,0 +1,37 @@
+//
+//  习题7-4 求矩阵各行元素之和.c
+//  PAT
+//
+//  Created by Vision on 2022/3/24.
+//
+
+#include<stdio.h>
+#define MANX 10
+
+int main(void)
+{
+    int m,n;
+    int sum = 0;
+    int a[MANX][MANX];
+    int b[MANX];
+    
+    scanf("%d %d", &m, &n);
+    for (int i = 0; i<m; i++) {
+        for (int j = 0; j<n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    for (int i = 0; i<m; i++) {
+        for (int j = 0; j<n; j++) {
+            sum = sum + a[i][j];
+        }
+        b[i] = sum;
+        sum = 0;
+    }
+    
+    for (int i = 0; i<m; i++) {
+        printf("%d\n",b[i]);
+    }
+    return 0;
+}
