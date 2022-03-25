17cd10f75cbb6074cfc259e95b489887f7b5ea46
Chris鸠 <948029240@qq.com>
2022-03-25 21:09:51 +0800

    Create 实验7-2-2 矩阵运算.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计实验与习题指导（第4版）》题目集/实验7-2-2 矩阵运算.c
@@ -0,0 +1,35 @@
+//
+//  实验7-2-2 矩阵运算.c
+//  PAT
+//
+//  Created by Vision on 2022/3/25.
+//
+
+#include<stdio.h>
+#define MANX 10
+
+int main(void)
+{
+    int n,result=0;
+    int a[MANX][MANX];
+    
+    scanf("%d", &n);
+    
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<n; j++) {
+            if (i+j!=n-1 && i!=n-1 && j!= n-1) {
+                result = result + a[i][j];
+            }
+        }
+    }
+    
+    printf("%d",result);
+    
+    return 0;
+}
