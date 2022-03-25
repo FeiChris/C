32a757969c34bc3131dc33c902f2e6a992d09977
Chris鸠 <948029240@qq.com>
2022-03-25 21:20:46 +0800

    Create 实验7-3-2 统计大写辅音字母.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计实验与习题指导（第4版）》题目集/实验7-3-2 统计大写辅音字母.c
@@ -0,0 +1,33 @@
+//
+//  实验7-3-2 统计大写辅音字母.c
+//  PAT
+//
+//  Created by Vision on 2022/3/25.
+//
+
+#include<stdio.h>
+#define Max 80
+
+int main(void)
+{
+    char c[Max];
+    int n=0;
+    int count=0;
+    
+    while ((c[n] = getchar())!= '\n') {
+        n++;
+    }
+    c[n]='\0';
+    
+    for (int i = 0; i<n; i++) {
+        if (c[i]>='A'&&c[i]<='Z') {
+            if (c[i]!='A' && c[i]!='E' && c[i]!='I' && c[i]!='O' && c[i]!='U') {
+                count++;
+            }
+        }
+    }
+    printf("%d",count);
+    
+    return 0;
+}
+
