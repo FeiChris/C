905fc6db31c3fc771c9e350fa72653d9a50759cd
Chris鸠 <948029240@qq.com>
2022-03-27 01:09:12 +0800

    Create 实验8-1-7 数组循环右移.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计实验与习题指导（第4版）》题目集/实验8-1-7 数组循环右移.c
@@ -0,0 +1,42 @@
+//
+//  实验8-1-7 数组循环右移.c
+//  PAT
+//
+//  Created by Vision on 2022/3/27.
+//
+
+#include <stdio.h>
+#define MAXN 10
+
+void ArrayShift( int a[], int n, int m );
+
+int main()
+{
+    int a[MAXN], n, m;
+    int i;
+    
+    scanf("%d %d", &n, &m);
+    for ( i = 0; i < n; i++ ) scanf("%d", &a[i]);
+    
+    ArrayShift(a, n, m);
+    
+    for ( i = 0; i < n; i++ ) {
+        if (i != 0) printf(" ");
+        printf("%d", a[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+void ArrayShift( int a[], int n, int m ){
+    int digit;
+    for (int i = 0; i<m; i++) {
+        digit = a[n-1];
+        for (int j = n-1; j>0; j--) {
+            a[j] = a[j-1];
+        }
+        a[0]=digit;
+    }
+}
