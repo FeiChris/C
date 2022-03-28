ebe4f0d5cfda6f806fd8d52b981bc1bdef86a521
Chris鸠 <948029240@qq.com>
2022-03-28 23:46:12 +0800

    Create 实验8-2-6 分类统计各类字符个数.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计实验与习题指导（第4版）》题目集/实验8-2-6 分类统计各类字符个数.c
@@ -0,0 +1,45 @@
+//
+//  实验8-2-6 分类统计各类字符个数.c
+//  PAT
+//
+//  Created by Vision on 2022/3/28.
+//
+
+#include <stdio.h>
+#define MAXS 15
+
+void StringCount( char *s );
+void ReadString( char *s ); /* 由裁判实现，略去不表 */
+
+int main()
+{
+    char s[MAXS];
+
+    ReadString(s);
+    StringCount(s);
+
+    return 0;
+}
+
+/* Your function will be put here */
+void StringCount( char *s ){
+    int letter_d,letter_x,blank,digit,other;
+    letter_d = 0;
+    letter_x= 0;
+    blank = 0;
+    digit = 0;
+    other = 0;
+    for (int i=0; s[i]!='\0'; i++) {
+        if (s[i]>=65 && s[i]<=90) {
+            letter_d++;
+        }else if (s[i] == 32){
+            blank++;
+        }else if (s[i]>='0' && s[i]<='9'){
+            digit++;
+        }else if(s[i]>=97 && s[i]<=122){
+            letter_x++;
+        }else
+            other++;
+    }
+    printf("%d %d %d %d %d",letter_d,letter_x,blank,digit,other);
+}
