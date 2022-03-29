a0a350050997a6a5e0d842a49e696e28102e667e
Chris鸠 <948029240@qq.com>
2022-03-29 19:54:48 +0800

    Create 习题8-1 拆分实数的整数与小数部分.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/习题8-1 拆分实数的整数与小数部分.c
@@ -0,0 +1,30 @@
+//
+//  习题8-1 拆分实数的整数与小数部分.c
+//  PAT
+//
+//  Created by Vision on 2022/3/29.
+//
+
+#include <stdio.h>
+
+void splitfloat( float x, int *intpart, float *fracpart );
+
+int main()
+{
+    float x, fracpart;
+    int intpart;
+    
+    scanf("%f", &x);
+    splitfloat(x, &intpart, &fracpart);
+    printf("The integer part is %d\n", intpart);
+    printf("The fractional part is %g\n", fracpart);
+    
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+
+void splitfloat( float x, int *intpart, float *fracpart ){
+    *intpart = (int)x;
+    *fracpart = x - *intpart;
+}
