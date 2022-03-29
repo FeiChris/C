36c6779057f61695728452e3c566682fd84c00ad
Chris鸠 <948029240@qq.com>
2022-03-29 19:52:20 +0800

    Create 练习8-2 计算两数的和与差.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/练习8-2 计算两数的和与差.c
@@ -0,0 +1,28 @@
+//
+//  练习8-2 计算两数的和与差.c
+//  PAT
+//
+//  Created by Vision on 2022/3/29.
+//
+
+#include <stdio.h>
+
+void sum_diff( float op1, float op2, float *psum, float *pdiff );
+
+int main()
+{
+    float a, b, sum, diff;
+
+    scanf("%f %f", &a, &b);
+    sum_diff(a, b, &sum, &diff);
+    printf("The sum is %.2f\nThe diff is %.2f\n", sum, diff);
+    
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+
+void sum_diff( float op1, float op2, float *psum, float *pdiff ){
+    *psum = op1 + op2;
+    *pdiff = op1 - op2;
+}
