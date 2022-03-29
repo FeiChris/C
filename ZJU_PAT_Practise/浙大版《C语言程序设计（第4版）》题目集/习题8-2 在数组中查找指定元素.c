5d586a7c7532f8c2a424ef6f41bd0d8ab5a84a08
Chris鸠 <948029240@qq.com>
2022-03-29 19:56:35 +0800

    Create 习题8-2 在数组中查找指定元素.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计（第4版）》题目集/习题8-2 在数组中查找指定元素.c
@@ -0,0 +1,41 @@
+//
+//  习题8-2 在数组中查找指定元素.c
+//  PAT
+//
+//  Created by Vision on 2022/3/29.
+//
+
+#include <stdio.h>
+#define MAXN 10
+
+int search( int list[], int n, int x );
+    
+int main()
+{
+    int i, index, n, x;
+    int a[MAXN];
+
+    scanf("%d", &n);
+    for( i = 0; i < n; i++ )
+        scanf("%d", &a[i]);
+    scanf("%d", &x);
+    index = search( a, n, x );
+    if( index != -1 )
+        printf("index = %d\n", index);
+    else
+        printf("Not found\n");
+            
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+int search( int list[], int n, int x ){
+    int result=-1;
+    for (int i = 0; i<n; i++) {
+        if (x==list[i]) {
+            result = i;
+            break;
+        }
+    }
+    return result;
+}
