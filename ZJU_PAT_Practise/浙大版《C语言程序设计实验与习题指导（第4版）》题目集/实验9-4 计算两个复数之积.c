fcdd90e07950e82aa12d2432a9ee7b6fc9856272
Chris鸠 <948029240@qq.com>
2022-04-05 18:18:01 +0800

    Create 实验9-4 计算两个复数之积.c

---
+++ ZJU_PAT_Practise/浙大版《C语言程序设计实验与习题指导（第4版）》题目集/实验9-4 计算两个复数之积.c
@@ -0,0 +1,35 @@
+//
+//  实验9-4 计算两个复数之积.c
+//  PAT
+//
+//  Created by Vision on 2022/4/5.
+//
+
+#include <stdio.h>
+
+struct complex{
+    int real;
+    int imag;
+};
+
+struct complex multiply(struct complex x, struct complex y);
+
+int main()
+{
+    struct complex product, x, y;
+
+    scanf("%d%d%d%d", &x.real, &x.imag, &y.real, &y.imag);
+    product = multiply(x, y);
+    printf("(%d+%di) * (%d+%di) = %d + %di\n",
+            x.real, x.imag, y.real, y.imag, product.real, product.imag);
+    
+    return 0;
+}
+
+/* 你的代码将被嵌在这里 */
+struct complex multiply(struct complex x, struct complex y){
+    struct complex product;
+    product.real = x.real * y.real - x.imag * y.imag;
+    product.imag = x.real * y.imag + x.imag * y.real;
+    return product;
+}
